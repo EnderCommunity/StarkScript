c781d3ed5993378e5cf61de6ebdf71a3b8247a81
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-06 20:39:54 +0200

    Update the start message function

--- compiler/message.c
+++ compiler/message.c
@@ -11,10 +11,10 @@
 #include "./info/language.c"
 
 // Define the function that prints the compiler start message
-void prntCmplrMsg(){
+void printCmplrMsg(){
 
     // Print any important compiler info
-    printf("StarkScript V%s (Compiler version %s | %s channel )\nUsing the [%s] compiler on %s\n\n\
+    consoleInfo("\nStarkScript V%s (Compiler version %s | %s channel )\nUsing the [%s] compiler on %s\n\n\
     * For more information, visit: %s\n\n\n",
     __STARK_VERSION__, __STARK_COMPILER_VERSION__, __STARK_RELEASE_STATE__, COMPILER_NAME,
     OPERATING_SYSTEM_MESSAGE_NAME, __STARK_WEBSITE_URL__);
