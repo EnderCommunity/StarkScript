9c7b4e7058b6094f24a6b10d3480480e296ce0df
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-06 20:38:11 +0200

    Add strings

---
+++ compiler/strings/console.c
@@ -0,0 +1,15 @@
+/**
+ *
+ *  Manage all the console strings
+ *
+**/
+
+
+// Define the prefix of debug console messages
+#define STRING_CONSOLE_DEBUG_MESSAGE "[*][Debug]: "
+
+// Define the prefix of warning console messages
+#define STRING_CONSOLE_WARNING_MESSAGE "[!][Warn]: "
+
+// Define the prefix of error console messages
+#define STRING_CONSOLE_ERROR_MESSAGE "[x][Error]: "
