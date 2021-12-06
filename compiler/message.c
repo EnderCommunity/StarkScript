874a7fda63b4268535fc1ec4b05048bc660c4a5c
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-06 15:47:01 +0200

    Update the compiler start message

--- compiler/message.c
+++ compiler/message.c
@@ -15,7 +15,7 @@ void prntCmplrMsg(){
 
     // Print any important compiler info
     printf("StarkScript V%s (Compiler version %s | %s channel )\nUsing the [%s] compiler on %s\n\n\
-    For more information, visit: %s\n\n\n",
+    * For more information, visit: %s\n\n\n",
     __STARK_VERSION__, __STARK_COMPILER_VERSION__, __STARK_RELEASE_STATE__, COMPILER_NAME,
     OPERATING_SYSTEM_MESSAGE_NAME, __STARK_WEBSITE_URL__);
 
