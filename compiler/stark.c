5c7dcae02cc792eff95ec4f390c13f6d126102e8
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-06 21:51:39 +0200

    Update the compiler to handle sudden terminations better

--- compiler/stark.c
+++ compiler/stark.c
@@ -8,6 +8,9 @@
 // Include the compiler flags
 #include "./flags.c"
 
+// Pre-define the `stopProcess` function so all the included files can use it
+void stopProcess(int exitCode);
+
 // Include all the console-related functions
 #include "./console.c"
 
@@ -26,14 +29,43 @@ int main(int argc, char *argv[]){
     // Process the command line arguments
     processArgs(argc, argv);
 
-    // ...
+    if(cmplrInputFilePth != NULL){
+
+        // ...
+
+        // Free the memory used by the `cmplrInputFilePth` variable
+        free(cmplrInputFilePth);
+
+    }else{
+
+        consoleError("No input file path was passed!\n");
+
+    }
 
     // Print a warning about the state of the compiler
     consoleWarn("This compiler is still not functional! Please, consider helping us!\n");
 
-    consoleError("Oops...\n");
-
     // Return a SUCCESS exit code
     return EXIT_SUCCESS;
+
+}
+
+// Define a function that stops the process
+void stopProcess(int exitCode){
+
+    // Free up any globally allocated variables depending on the compiling stage that the error occured in
+    if(cmplrInputFilePth != NULL) {
+
+        free(cmplrInputFilePth);
+
+    }
+    if(cmplrOutputFilePth != NULL){
+
+        free(cmplrInputFilePth);
+
+    }
+
+    // Use the built-in `exit` function to terminate the process
+    exit(exitCode);
 
 }
