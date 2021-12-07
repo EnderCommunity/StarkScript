fb479ae925199e178539ac461dc25ec9ac2a3a53
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-07 17:59:38 +0200

    Add colours and update the console messages prefixes

--- compiler/strings/console.c
+++ compiler/strings/console.c
@@ -5,11 +5,23 @@
 **/
 
 
+// Define the console colours
+#define CONSOLE_COLOR_RESET "\033[0m"
+#define CONSOLE_COLOR_BLACK "\033[0;30m"
+#define CONSOLE_COLOR_RED "\033[0;31m"
+#define CONSOLE_COLOR_GREEN "\033[0;32m"
+#define CONSOLE_COLOR_YELLOW "\033[0;33m"
+#define CONSOLE_COLOR_BLUE "\033[0;34m"
+#define CONSOLE_COLOR_PURPLE "\033[0;35m"
+#define CONSOLE_COLOR_CYAN "\033[0;36m"
+#define CONSOLE_COLOR_WHITE "\033[0;37m"
+#define CONSOLE_COLOR_GRAY "\e[0;90m"
+
 // Define the prefix of debug console messages
-#define STRING_CONSOLE_DEBUG_MESSAGE "[*][Debug]: "
+#define STRING_CONSOLE_DEBUG_MESSAGE "[\033[0;36m*\033[0m][\033[0;36mDebug\033[0m]: "
 
 // Define the prefix of warning console messages
-#define STRING_CONSOLE_WARNING_MESSAGE "[!][Warn]: "
+#define STRING_CONSOLE_WARNING_MESSAGE "[\033[0;33m!\033[0m][\033[0;33mWarn\033[0m]: "
 
 // Define the prefix of error console messages
-#define STRING_CONSOLE_ERROR_MESSAGE "[x][Error]: "
+#define STRING_CONSOLE_ERROR_MESSAGE "[\033[0;31mx\033[0m][\033[0;31mError\033[0m]: "
