7610b0aecdd8c3492e3a89bf5cd58e4389bf453f
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-09 10:52:51 +0200

    Update the initial compiling files

--- compiler/initial/initial.c
+++ compiler/initial/initial.c
@@ -15,3 +15,28 @@
 // The lexer is the part of the compiler that takes care of listing, identifying, and assigning
 // types to every word and character within the "mega input file"...
 #include "./lexer/lexer.c"
+
+// Define a function that triggers the initial part of compiling
+void initialComp(struct GlobalIO globalIO){
+
+    // Open a file stream in read-only mode to read the content of the input file
+    FILE *inputFile = fopen(globalIO.input.fullPth, "r");
+
+    // Check if the file stream wasn't opened successfully
+    if(inputFile == NULL){
+
+        // Tell the user that the input file is inaccessible
+        consoleError("Couldn't access the input file!");
+
+    }
+
+    // Trigger the preprocessor
+    preproc(inputFile);
+    
+    // Trigger the lexer
+    lexer();
+
+    // Close the file stream
+    fclose(inputFile);
+
+}
