161b422bb3b0b1d03e53f287efa222a5b74185ea
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-12 16:07:18 +0200

    Add the `SYSTEM_COMMAND_MOVE_FILE` constant

--- compiler/strings/system.h
+++ compiler/strings/system.h
@@ -8,12 +8,25 @@
 // Define the command that can be used to remove full folders
 #ifdef _WIN32
 
-    // For more info about the "rmdir" for Windows, visit:
+    // For more info about the "rmdir" command for Windows, visit:
     // https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/rmdir
     #define SYSTEM_COMMAND_REMOVE_FULL_DIR "rmdir /s /q "
 
 #else
 
     #define SYSTEM_COMMAND_REMOVE_FULL_DIR "rm -rf "
+
+#endif
+
+// Define the command that can be used to move files
+#ifdef _WIN32
+
+    // For more info about the "move" command for Windows, visit:
+    // https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/move
+    #define SYSTEM_COMMAND_MOVE_FILE "move /y "
+
+#else
+
+    #define SYSTEM_COMMAND_MOVE_FILE "mv "
 
 #endif
