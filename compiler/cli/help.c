7e6a344efb6503674e795ad10a78f51fbb7b9b30
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-12 19:14:51 +0200

    Update the help catalog

--- compiler/cli/help.c
+++ compiler/cli/help.c
@@ -14,6 +14,8 @@ void helpCatalog(){
     // You must never add any whitespace in this part of this file
 "%sUsage%s: Stark [options]\n\
 %sOptions%s:\n\
+    %s--help%s                   View the help catalog\n\
+                                 (This option will prevent the compiler from receiving any more command line arguments, and any already-passed arguments will be ignored)\n\
     %s--input%s <path>           Pass the path of your input file\n\
     %s--output%s <directory>     Pass the output directory of your compiled program\n\
                                  (Defaults to the input file directory)\n\
@@ -24,6 +26,7 @@ void helpCatalog(){
     %s--save-temps%s             Keep intermediate files\n\
     %s--save-temps-c%s           Keep C intermediate files\n\
     %s--gcc%s                    Runs the \"gcc -v\" command\n\
+    %s-h%s <path>                Same as --help\n\
     %s-i%s <path>                Same as --input\n\
     %s-o%s <directory>           Same as --output\n\
     %s-n%s <string>              Same as --name\n\n\
@@ -41,8 +44,10 @@ For bug reporting, please visit: %sissues/\n",
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
+    CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_RED, CONSOLE_COLOR_RESET, // Colour the `--save-temps-c` flag with red to indicate
                                             // that it's an incomplete flag!
+    CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
     CONSOLE_COLOR_PURPLE, CONSOLE_COLOR_RESET,
