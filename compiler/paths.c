a16b972c7956bbb909b02a0ed60f81989263a536
EnderAdel <adel.sbeh.email@gmail.com>
2021-12-13 12:13:25 +0200

    Add the `joinDirFile` function

--- compiler/paths.c
+++ compiler/paths.c
@@ -25,3 +25,24 @@ char* joinDirFileExt(char *dir, char *fileName, char *ext){
     return fullPth;
 
 }
+
+// Define a function that can join a directory's path with a file name
+// ${dir}(/|\\)${fileName}
+char* joinDirFile(char *dir, char *fileName){
+
+    // Allocate memory for the return string
+    char *fullPth = calloc(strlen(dir) + 1 + strlen(fileName) + 1, sizeof(char));
+
+    // Combine the path values
+    sprintf(fullPth, "%s%c%s", dir,
+    #ifdef _WIN32
+        '\\'
+    #else
+        '/'
+    #endif
+    , fileName);
+
+    // Return the full path
+    return fullPth;
+
+}
