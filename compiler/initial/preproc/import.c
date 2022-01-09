9d6774c254bc6cbc01df06e19d435fa2a76988da
0xENDER <adel.sbeh.email@gmail.com>
2022-01-09 23:50:18 +0200

    Update the `import` statement

--- compiler/initial/preproc/import.c
+++ compiler/initial/preproc/import.c
@@ -141,9 +141,9 @@ int importContext(FILE **inputFile, char *prvsChar, char *currChar, int *column)
 }
 
 // Define a function that can inject a file's content into the "mega" input file
-void injectImportFile(FILE *inputFile, FILE *outputFile, int *processedFiles, const char *codePath,
+void injectFile(FILE *inputFile, FILE *outputFile, int *processedFiles, const char *codePath,
                         const char *filePath, int line, int column, char *importPath,
-                        char *absImportPath){
+                        char *absImportPath, const char *libsDir){
 
     // Inject the opening guiding line
     fprintf(outputFile, "\n#file %s ? %s\n", importPath, absImportPath);
@@ -152,7 +152,7 @@ void injectImportFile(FILE *inputFile, FILE *outputFile, int *processedFiles, co
     char *importCodePath = createCodePath(codePath, filePath, line, column);
 
     // Open the imported file
-    FILE *importFile = fopen(importPath, "r");
+    FILE *importFile = fopen(absImportPath, "r");
 
     // Check if the file stream wasn't opened successfully
     if(importFile == NULL){
@@ -167,7 +167,7 @@ void injectImportFile(FILE *inputFile, FILE *outputFile, int *processedFiles, co
     }else{
 
         // Start injecting the content of the import file
-        preprocR(importFile, outputFile, processedFiles, importCodePath, importPath);
+        preprocR(importFile, outputFile, processedFiles, importCodePath, importPath, libsDir);
 
         // Close the opened stream to the imported file
         fclose(importFile);
@@ -185,124 +185,122 @@ void injectImportFile(FILE *inputFile, FILE *outputFile, int *processedFiles, co
 // Define a function that will return the output of an import narrative
 void outputImport(FILE **inputFile, FILE **outputFile, char *currChar, int *injectionWhitespace,
                     int line, int *column, const char *codePath, const char *filePath, int inQuote,
-                    int *inDoubleQuote, int *importContextStart, int *processedFiles){
+                    int *inDoubleQuote, int *importContextStart, int *processedFiles, const char *libsDir){
 
-                    // Print a white space character for each character you deal with to keep the
-                    // column value intact
-                    (*injectionWhitespace) += 6;
+    // Print a white space character for each character you deal with to keep the column value
+    // intact
+    (*injectionWhitespace) += 6;
 
-                    // Check if you're in a new line now
-                    if(*currChar == '\n' || *currChar == '\r'){
+    // Check if you're in a new line now
+    if(*currChar == '\n' || *currChar == '\r'){
 
-                        // Temp
-                        preprocError(*inputFile, *outputFile, STR_ERROR_000002, codePath, filePath,
-                                        line, *column, 1, 1);
+        // Temp
+        preprocError(*inputFile, *outputFile, STR_ERROR_000002, codePath, filePath, line, *column,
+                        1, 1);
 
-                    }else if(inQuote){ // Check if you're in a quoting context
+    }else if(inQuote){ // Check if you're in a quoting context
 
-                        // Temp
-                        preprocError(*inputFile, *outputFile, STR_ERROR_000003, codePath, filePath,
-                                        line, *column, 1, 0);
+        // Temp
+        preprocError(*inputFile, *outputFile, STR_ERROR_000003, codePath, filePath, line, *column,
+                        1, 0);
 
-                    }else if(*inDoubleQuote){ // Check if you're in a double-quoting context
+    }else if(*inDoubleQuote){ // Check if you're in a double-quoting context
 
-                        // Keep track of the import start column value for reports
-                        int startColumn = *column;
+        // Keep track of the import start column value for reports
+        int startColumn = *column;
 
-                        // Get the first character in the quoting context
-                        (*injectionWhitespace)++;
-                        *currChar = fgetc(*inputFile);
-                        (*column)++;
+        // Get the first character in the quoting context
+        (*injectionWhitespace)++;
+        *currChar = fgetc(*inputFile);
+        (*column)++;
 
-                        // Skip whitespace
-                        while(*currChar == ' '){
+        // Skip whitespace
+        while(*currChar == ' '){
 
-                            // Get the next character
-                            (*injectionWhitespace)++;
-                            *currChar = fgetc(*inputFile);
-                            (*column)++;
+            // Get the next character
+            (*injectionWhitespace)++;
+            *currChar = fgetc(*inputFile);
+            (*column)++;
 
-                        }
+        }
 
-                        // Prepare the path string
-                        int importPathSize = 1;
-                        char *importPath = calloc(importPathSize, sizeof(char));
+        // Prepare the path string
+        int importPathSize = 1;
+        char *importPath = calloc(importPathSize, sizeof(char));
 
-                        // Get the import path
-                        while(*currChar != '"'){
+        // Get the import path
+        while(*currChar != '"'){
 
-                            // Check if this is a new line!
-                            if(*currChar == '\r' || *currChar == '\n'){
+            // Check if this is a new line!
+            if(*currChar == '\r' || *currChar == '\n'){
 
-                                preprocError(*inputFile, *outputFile, STR_ERROR_000007, codePath,
-                                                filePath, line, (*column) - 1, 1, 1);
+                preprocError(*inputFile, *outputFile, STR_ERROR_000007, codePath, filePath, line,
+                                (*column) - 1, 1, 1);
 
-                            }
+            }
 
-                            // Copy the current character
-                            importPath = realloc(importPath, (++importPathSize)*sizeof(char));
-                            importPath[importPathSize - 2] = *currChar;
+            // Copy the current character
+            importPath = realloc(importPath, (++importPathSize)*sizeof(char));
+            importPath[importPathSize - 2] = *currChar;
 
-                            // Get the next character
-                            (*injectionWhitespace)++;
-                            *currChar = fgetc(*inputFile);
-                            (*column)++;
+            // Get the next character
+            (*injectionWhitespace)++;
+            *currChar = fgetc(*inputFile);
+            (*column)++;
 
-                        }
-                        importPath[importPathSize - 1] = '\0';
+        }
+        importPath[importPathSize - 1] = '\0';
 
-                        // Get the next character
-                        (*inDoubleQuote) = 0;
-                        (*injectionWhitespace)++;
-                        *currChar = fgetc(*inputFile);
-                        (*column)++;
+        // Get the next character
+        (*inDoubleQuote) = 0;
+        (*injectionWhitespace)++;
+        *currChar = fgetc(*inputFile);
+        (*column)++;
 
-                        // Skip whitespace
-                        while(*currChar == ' '){
+        // Skip whitespace
+        while(*currChar == ' '){
 
-                            // Get the next character
-                            (*injectionWhitespace)++;
-                            *currChar = fgetc(*inputFile);
-                            (*column)++;
+            // Get the next character
+            (*injectionWhitespace)++;
+            *currChar = fgetc(*inputFile);
+            (*column)++;
 
-                        }
+        }
 
-                        if(*currChar != ';'){
+        if(*currChar != ';'){
 
-                            preprocError(*inputFile, *outputFile, STR_ERROR_000004, codePath,
-                                            filePath, line, *column, 1, 0);
+            preprocError(*inputFile, *outputFile, STR_ERROR_000004, codePath,
+                            filePath, line, *column, 1, 0);
 
-                        }else{
+        }else{
 
-                            consoleDebug("Import path detected: %s", importPath);
+            consoleDebug("Import path detected: %s", importPath);
 
-                            // Get the absolute path of the import file
-                            char *absoluteImportPath = absPth(importPath);
+            // Get the absolute path of the import file
+            char *absoluteImportPath = absPth(importPath);
 
-                            // Inject the import file content
-                            injectImportFile(*inputFile, *outputFile, processedFiles, codePath,
-                                                filePath, line, startColumn, importPath,
-                                                absoluteImportPath);
+            // Inject the import file content
+            injectFile(*inputFile, *outputFile, processedFiles, codePath, filePath, line,
+                                startColumn, importPath, absoluteImportPath, libsDir);
 
-                            // Free up the memory used by the `absoluteImportPath` and `importPath`
-                            // variables
-                            free(absoluteImportPath);
-                            free(importPath);
+            // Free up the memory used by the `absoluteImportPath` and `importPath` variables
+            free(absoluteImportPath);
+            free(importPath);
 
-                            // Set the import context status to "false"
-                            (*importContextStart) = 0;
+            // Set the import context status to "false"
+            (*importContextStart) = 0;
 
-                            // Replace the current character with an espace character
-                            *currChar = CHAR_SPECIAL_ESCAPE[0];
+            // Replace the current character with an espace character
+            *currChar = CHAR_SPECIAL_ESCAPE[0];
 
-                        }
+        }
 
-                    }else if(*currChar != ' '){
+    }else if(*currChar != ' '){
 
-                        // Temp
-                        preprocError(*inputFile, *outputFile, STR_ERROR_000004, codePath, filePath,
-                                        line, *column, 1, 0);
+        // Temp
+        preprocError(*inputFile, *outputFile, STR_ERROR_000004, codePath, filePath, line, *column,
+                        1, 0);
 
-                    }
+    }
 
 }
