366e34606ed191507e675f24f67e43a020c30e2c
0xENDER <adel.sbeh.email@gmail.com>
2022-01-09 23:48:38 +0200

    Update some error messages

--- compiler/strings/report.h
+++ compiler/strings/report.h
@@ -10,11 +10,13 @@
 #define STR_ERROR_000001 "A 'use' context should not include a quoting context!"
 #define STR_ERROR_000002 "You cannot move to a new line within an 'import' context!"
 #define STR_ERROR_000003 "You can only use double quotes to point out strings!"
-#define STR_ERROR_000004 "Unexpected 'import' context!"
+#define STR_ERROR_000004 "Invalid 'import' syntax!"
 #define STR_ERROR_000005 "You cannot move to a new line within a 'use' context!"
-#define STR_ERROR_000006 "You can only start 'use' input with a character or an underscore!"
+#define STR_ERROR_000006 "Invalid 'use' syntax! (Must start with a character, and may only contain characters and numbers)"
 #define STR_ERROR_000007 "You cannot move to a new line inside a string context!"
-#define STR_ERROR_000008 "An invalid/inaccessible path was passed to the 'import' function!"
+#define STR_ERROR_000008 "Attempting to access an invalid/inaccessible path!"
+#define STR_ERROR_000009 "Invalid 'use' syntax! (A dot may only be followed by a character)"
+#define STR_ERROR_000010 "Invalid 'use' syntax! (You must separate symbols with a dot)"
 
 // Define the strings for warnings
 #define STR_WARN_000000 ""
