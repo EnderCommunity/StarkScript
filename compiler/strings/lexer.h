4eb7d211f39b5d9ab399e140d4986a248407a51e
0xENDER <adel.sbeh.email@gmail.com>
2022-01-16 02:36:26 +0200

    Add lexer strings

---
+++ compiler/strings/lexer.h
@@ -0,0 +1,40 @@
+/**
+ *
+ *  Manage all the lexer-related strings
+ *
+**/
+
+
+// KEYWORD strings
+#define LEXER_KEYWORD_EXIT_PROGRAM "exit"
+#define LEXER_KEYWORD_SET_MAIN_FUNCTION "execute"
+#define LEXER_KEYWORD_DEFINE_FREE_CONSTANT "define"
+#define LEXER_KEYWORD_MAKE_NEW "new"
+#define LEXER_KEYWORD_DELETE_VARIABLE "delete"
+#define LEXER_KEYWORD_RETURN_VALUE "return"
+
+// DEFINITIONAL-KEYWORD strings
+#define LEXER_DEFINITIONAL_KEYWORD_DEFINE_VARIABLE "variable"
+#define LEXER_DEFINITIONAL_KEYWORD_DEFINE_CONSTANT "constant"
+#define LEXER_DEFINITIONAL_KEYWORD_DEFINE_FUNCTION "function"
+
+// CONDITIONAL-KEYWORD strings
+#define LEXER_KEYWORD_CONDITIONAL_IF "if"
+#define LEXER_KEYWORD_CONDITIONAL_ELSE "else"
+
+// TYPE-KEYWORD strings
+#define LEXER_TYPE_KEYWORD_BOOL "bool"
+#define LEXER_TYPE_KEYWORD_INT "int"
+#define LEXER_TYPE_KEYWORD_FLOAT "float"
+#define LEXER_TYPE_KEYWORD_DOUBLE "double"
+#define LEXER_TYPE_KEYWORD_CHAR "char"
+#define LEXER_TYPE_KEYWORD_STRING "string"
+#define LEXER_TYPE_KEYWORD_SHORT "short"
+#define LEXER_TYPE_KEYWORD_LONG "long"
+#define LEXER_TYPE_KEYWORD_VOID "void"
+#define LEXER_TYPE_KEYWORD_SIGNED "signed"
+#define LEXER_TYPE_KEYWORD_UNSIGNED "unsigned"
+
+// BOOLEAN strings
+#define LEXER_BOOLEAN_TRUE "true"
+#define LEXER_BOOLEAN_FALSE "false"
