56b5f3cf9712720bd5a5f485889a3a1a7eefd88a
0xENDER <adel.sbeh.email@gmail.com>
2022-01-16 02:36:12 +0200

    Add the lexer's extension

--- compiler/strings/io.h
+++ compiler/strings/io.h
@@ -10,3 +10,6 @@
 
 // Define the extension of the preprocessor's output file (the "mega file")
 #define STRING_IO_PREPROCESSOR_OUTPUT_EXT "input"
+
+// Define the extension of the lexer's output file
+#define STRING_IO_LEXER_OUTPUT_EXT "lexicon"
